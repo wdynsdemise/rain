aefd3b2c531d92ae7df826764e10fd706eec8abc
wdynsdemise <asdojasduoh@gmail.com>
2025-08-19 14:05:49 -0700

    loader

---
+++ loader.lua
@@ -0,0 +1,3 @@
+-- loader
+-- ==========================
+loadstring(game:HttpGet('https://gist.githubusercontent.com/wdynsdemise/144776629a2abf6cc2b28ee5f6c49001/raw/28b92566f115753cd01de64346bbe83f9dd90c02/gistfile1.txt'))()
