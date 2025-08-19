91cd1fcd393b5a75b298786aecba28cc4bf44b9a
wdynsdemise <asdojasduoh@gmail.com>
2025-08-19 14:46:08 -0700

    Delete loader.lua

--- loader.lua
+++
@@ -1,3 +0,0 @@
--- loader
--- ==========================
-loadstring(game:HttpGet('https://gist.githubusercontent.com/wdynsdemise/144776629a2abf6cc2b28ee5f6c49001/raw/28b92566f115753cd01de64346bbe83f9dd90c02/gistfile1.txt'))()
